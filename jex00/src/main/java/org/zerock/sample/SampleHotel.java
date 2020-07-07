4699357f2fb09a35619590bd0d95e7ba8eb66f25
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-07-07 22:25:00 +0900

    스프링 Java 설정

---
+++ jex00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,22 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor
+public class SampleHotel {
+
+	
+	private Chef chef;
+
+
+	
+}
