167b6ece1c26ea424d31c822b68c8aa8ed64eaf9
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-07-14 13:26:57 +0900

    [CREATE] MyBatis mapper interface

---
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,34 @@
+package org.zerock.mapper;
+
+//import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.zerock.domain.BoardVO;
+//import org.zerock.domain.Criteria;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+
+@ContextConfiguration(classes = {org.zerock.config.RootConfig.class} )
+@Log4j
+public class BoardMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	@Test
+	public void testGetList() {
+
+		mapper.getList().forEach(board -> log.info(board));
+
+	}
+
+
+}
