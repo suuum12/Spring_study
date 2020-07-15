16fd8936407c0143ec6a9e4b5547994de598301b
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-07-15 12:54:54 +0900

    [CREATE] create, read, delete 처리

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -8,5 +8,12 @@ public interface BoardMapper {
 	
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
-
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //import org.zerock.domain.BoardVO;
 //import org.zerock.domain.Criteria;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -29,6 +30,47 @@ public class BoardMapperTests {
 		mapper.getList().forEach(board -> log.info(board));
 
 	}
+	
+//	@Test
+//	public void testInsert() {
+//
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글");
+//		board.setContent("새로 작성하는 내용");
+//		board.setWriter("newbie");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//
+//	}
+//	
+//	@Test
+//	public void testInsertSelectKey() {
+//
+//		BoardVO board = new BoardVO();
+//		board.setTitle("마지막 작성하는 글 select key");
+//		board.setContent("마지막 작성하는 내용 select key");
+//		board.setWriter("last");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//
+//	}
+	
+	@Test
+	public void testRead() {
 
+		BoardVO board = mapper.read(5L); //long형 타입이라는 것을 명시
+		
+		log.info(board);
 
+	}
+
+	@Test
+	public void testDelete() {
+		
+		log.info("DELETE COUNT: " + mapper.delete(3L));
+	}
 }
