7e58a308d5bdddfa738a7dc4db3a77df9129a75b
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-08-24 20:32:27 +0900

    jsp 화면 처리 구현

---
+++ Prac01/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -0,0 +1,97 @@
+package org.zerock.controller;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.queryParam;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+// Test for Controller
+@WebAppConfiguration
+
+@ContextConfiguration(classes = { org.zerock.config.RootConfig.class, org.zerock.config.ServletConfig.class} )
+@Log4j
+public class BoardControllerTests {
+
+	@Setter(onMethod_ = { @Autowired })
+	private WebApplicationContext ctx;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+
+	@Test
+	public void testList() throws Exception {
+
+		log.info(
+				mockMvc
+				.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/get")
+				.param("bno", "5")
+			).andReturn().getModelAndView().getModelMap());
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새 글 제목")
+				.param("content", "테스트 새 글 내용")
+				.param("writer", "테스트 유저")
+			).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("bno", "8")
+				.param("title", "수정 글 제목")
+				.param("content", "수정 글 내용")
+				.param("writer", "수정한 유저")
+			).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+		
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno", "10")
+			).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+		
+	}
+
+
+}
+
+
---
+++ Prac01/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,92 @@
+package org.zerock.mapper;
+
+//import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.zerock.domain.BoardVO;
+//import org.zerock.domain.Criteria;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+
+@ContextConfiguration(classes = {org.zerock.config.RootConfig.class} )
+@Log4j
+public class BoardMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	@Test
+	public void testGetList() {
+
+		mapper.getList().forEach(board -> log.info(board));
+
+	}
+	
+//	@Test
+//	public void testInsert() {
+//
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글");
+//		board.setContent("새로 작성하는 내용");
+//		board.setWriter("newbie");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//
+//	}
+//	
+//	@Test
+//	public void testInsertSelectKey() {
+//
+//		BoardVO board = new BoardVO();
+//		board.setTitle("마지막 작성하는 글 select key");
+//		board.setContent("마지막 작성하는 내용 select key");
+//		board.setWriter("last");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//
+//	}
+	
+	@Test
+	public void testRead() {
+
+		BoardVO board = mapper.read(5L); //long형 타입이라는 것을 명시
+		
+		log.info(board);
+
+	}
+
+//	@Test
+//	public void testDelete() {
+//		
+//		log.info("DELETE COUNT: " + mapper.delete(3L));
+//	}
+	
+	@Test
+	public void testUpdate() {
+
+		BoardVO board = new BoardVO(); 
+		// 실행 전 존재하는 번호인지 확인
+		board.setBno(6L);
+		board.setTitle("수정한 제목");
+		board.setContent("수정한 내용");
+		board.setWriter("New user");
+		
+		int count = mapper.update(board);
+		
+		log.info("UPDATE COUNT: " + count);
+
+	}
+}
